fb1a1d005bc4846b992c7a619e19e8829603bb68
GFleishman <greg.nli10me@gmail.com>
2024-02-27 14:10:17 -0500

    bump version for pypi rebuild

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ClusterWrap",
-    version="0.3.0",
+    version="0.3.1",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Wrappers around dask-jobqueue functions for specific clusters",
