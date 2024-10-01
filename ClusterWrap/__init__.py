97ca57f806b37d8b065ce79c78d48745d46e0768
Johan <johan@e11.bio>
2024-10-01 23:58:53 +0000

    supress output of bsub on import

--- ClusterWrap/__init__.py
+++ ClusterWrap/__init__.py
@@ -5,7 +5,7 @@ from .clusters import janelia_lsf_cluster, local_cluster
 cluster = local_cluster
 
 if which('bsub') is not None:
-    if os.system('bsub -V') != 32512:
+    test_version = os.system('bsub -V > /dev/null 2>&1')
+    # Check if version call was succesful (32512: fail, 512: Unknown option).
+    if (test_version != 32512) | (test_version != 512):
         cluster = janelia_lsf_cluster
-
-
